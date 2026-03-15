9d8cf4afc8102c7010c2276fa1ce9e3cf65c6613
vg345 <vanshikag345@gmail.com>
2026-03-15 05:43:50 +0000

    animation, music

--- scripts/item.gd
+++ scripts/item.gd
@@ -4,15 +4,20 @@ extends Node2D
 var data:Dictionary
 var init = false
 
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("player"):
 		if  data[my_type]["hurt_by"].has(area.get_parent().current_mode):
 			global.score += 1
+			%AudioStreamPlayer2D.play()
+			%AnimationPlayerSpawn.play("despawn")
+			await %AnimationPlayerSpawn.animation_finished
 			queue_free()
 
 func _ready():
 	var json = FileAccess.get_file_as_string("res://scripts/data.json")
 	data = JSON.parse_string(json)
+	%AnimationPlayerSpawn.play("spawn")
 	
 func _process(_delta):
 	if !init and my_type != null:
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 var item = preload("res://scenes/item.tscn")
 var item_types = ["rock", "paper", "scissors"]
+var one_time = true
 
 func _process(_delta):
 	%score.text = str(global.score)
@@ -20,14 +21,19 @@ func _process(_delta):
 			%player.global_position.x = min(%pt1.global_position.x + 700, new_x)
 		if Input.is_action_just_pressed("stop"):
 			%player.reset()
-	if global.game_over:
+	if global.game_over and one_time:
 		game_is_over()
+		one_time = false
 
 func game_is_over():
 	if global.score > global.high_score:
 		global.high_score = global.score
 		%new_hs.visible = true
 	%spawnTimer.stop()
+	%AudioStreamPlayer2D.stop()
+	%AudioStreamGameOver.play()
+	if %AudioStreamGameOver.stream_paused:
+		%AudioStreamGameOver.stream_paused = false
 	global.score = 0
 	%over.visible = true
 
@@ -51,3 +57,37 @@ func shift_items():
 func _on_restart_button_pressed() -> void:
 	global.game_over = false
 	get_tree().reload_current_scene()
+
+
+func _on_down_button_pressed() -> void:
+	var new_y = %player.global_position.y + 100
+	%player.global_position.y = min(%pt1.global_position.y + 700, new_y)
+
+
+func _on_left_button_pressed() -> void:
+	var new_x = %player.global_position.x - 100
+	%player.global_position.x = max(%pt1.global_position.x, new_x)
+
+
+func _on_right_button_pressed() -> void:
+	var new_x = %player.global_position.x + 100
+	%player.global_position.x = min(%pt1.global_position.x + 700, new_x)
+
+
+func _on_reset_button_pressed() -> void:
+	%player.reset()
+
+func _on_up_button_pressed() -> void:
+	var new_y = %player.global_position.y - 100
+	%player.global_position.y = max(%pt1.global_position.y, new_y)
+
+
+func _on_close_inst_pressed() -> void:
+	%Instructions.visible = false
+
+
+func _on_mute_pressed() -> void:
+	if %AudioStreamPlayer2D.playing:
+		%AudioStreamPlayer2D.stop()
+	else:
+		%AudioStreamPlayer2D.play()
